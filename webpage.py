79e026560c120d807c36d2546ec96f6179520255
Luca <lucaccorey@gmail.com>
2019-11-21 10:16:44 -0800

    Added formatting

---
+++ webpage.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, Markup, render_template, flash, Markup
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def intro():
+    with open('primary elections 2016.json') as election_data:
+        electionData = json.load(election_data)
+    return render_template('Political Demographic Intro.html')
+
+if __name__=="__main__":
+    app.run(debug=False)
