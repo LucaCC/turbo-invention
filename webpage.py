fb344570c19fcc571a433dc7bf7a4cc46fb5f5c9
Luca <lucaccorey@gmail.com>
2019-12-02 09:43:29 -0800

    Trying to fix CSS & Beatify

--- webpage.py
+++ webpage.py
@@ -4,11 +4,13 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def intro():
     with open('primary elections 2016.json') as election_data:
         electionData = json.load(election_data)
     return render_template('Political_Demographic_Intro.html')
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=False)
