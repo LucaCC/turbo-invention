b719fc20ce66d1fed90eda46421701d69f6a6324
Luca <lucaccorey@gmail.com>
2019-12-06 10:01:31 -0800

    Added Graphs

--- webpage.py
+++ webpage.py
@@ -13,7 +13,10 @@ def intro():
 def First_Page():
     with open('Primary_Elections_2016.json') as election_data:
         electionData = json.load(election_data)
-    return render_template('Political_Demographic_Dropdown.html', candidate=get_candidate_options(electionData), c_info=political_party(request.args['candidate'], electionData), c=request.args['candidate'])
+        if 'candidate' in request.args:
+            return render_template('Political_Demographic_Dropdown.html', candidate=get_candidate_options(electionData), c_info=political_party(request.args['candidate'], electionData), c=request.args['candidate'], repNum=get_party("Republican", electionData), demNum=get_party("Democrat", electionData), Num=get_party("N/A", electionData))
+        else:
+            return render_template('Political_Demographic_Dropdown.html', candidate=get_candidate_options(electionData), repNum=get_party("Republican", electionData), demNum=get_party("Democrat", electionData), Num=get_party("N/A", electionData), repNumVote=get_party_vote("Republican", electionData), demNumVote=get_party_vote("Democrat", electionData), NumVote=get_party_vote("N/A", electionData))
 
 def get_candidate_options(election_data):
     candidate = []
@@ -25,17 +28,37 @@ def get_candidate_options(election_data):
     for data in candidate:
         options = options + \
             Markup("<option value=\"" + data + "\">" + data + "</option>")
+    return options
 
 def political_party(candidate_chosen, election_data):
     political_party = ""
     key = "Political Party: "
     for data in election_data:
-        if data["Vote Data"] == candidate_chosen:
-            political_party = data["Vote Data"]["Party"]
+        for a in data["Vote Data"]:
+            if a == candidate_chosen:
+                political_party = data["Vote Data"][a]["Party"]
 
     return key + str(political_party)
 
-    return options
+def get_party(party_type, election_data):
+    party = []
+    count = 0
+    for data in election_data:
+        for a in data["Vote Data"]:
+            if a not in party and data["Vote Data"][a]["Party"] == party_type:
+                count+=1
+                party.append(a)
+    return count
+
+def get_party_vote(party_type, election_data):
+    num_votes = 0
+    for data in election_data:
+        for a in data["Vote Data"]:
+            if data["Vote Data"][a]["Party"] == party_type:
+                num_votes += data["Vote Data"][a]["Number of Votes"]
+    return num_votes
+
+
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
