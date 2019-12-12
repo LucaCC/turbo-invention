265cf43f404ba24a4609688393fd80ed8792d395
Luca <lucaccorey@gmail.com>
2019-12-12 10:16:50 -0800

    Chart Works need to add color

--- webpage.py
+++ webpage.py
@@ -13,38 +13,38 @@ def intro():
 def First_Page():
     with open('Primary_Elections_2016.json') as election_data:
         electionData = json.load(election_data)
+        demColors = ["#37c732", "#43bfe6", "#4141f0"]
+        repColors = ["#37c732", "#43bfe6", "#4141f0", "#37c732", "#43bfe6", "#4141f0","#37c732", "#43bfe6", "#4141f0"]
         if 'candidate' in request.args:
             return render_template('Political_Demographic_Dropdown.html', candidate=get_candidate_options(electionData),
             c_info=political_party(request.args['candidate'], electionData), c=request.args['candidate'],
             repNum=get_party("Republican", electionData), demNum=get_party("Democrat", electionData), Num=get_party("N/A", electionData),
             repNumVote=get_party_vote("Republican", electionData),demNumVote=get_party_vote("Democrat", electionData),
-            NumVote=get_party_vote("N/A", electionData))
+            NumVote=get_party_vote("N/A", electionData), dDataPoints=get_cand_vote_data("Democrat", electionData),
+            rDataPoints=get_cand_vote_data("Republican", electionData))
         else:
             return render_template('Political_Demographic_Dropdown.html', candidate=get_candidate_options(electionData),
             repNum=get_party("Republican", electionData), demNum=get_party("Democrat", electionData),
             Num=get_party("N/A", electionData), repNumVote=get_party_vote("Republican", electionData),
             demNumVote=get_party_vote("Democrat", electionData), NumVote=get_party_vote("N/A", electionData),
-            dDataPoints=get_cand_vote_data("Democrat", election_data), rDataPoints=get_cand_vote_data("Republican", election_data))
+            dDataPoints=get_cand_vote_data("Democrat", electionData, demColors), rDataPoints=get_cand_vote_data("Republican", electionData, repColors))
 
-def get_cand_vote_data(party, election_data):
-    candidate = []
-    print(election_data)
+def get_cand_vote_data(party, election_data, colors):
+    candidate = {}
+    count = 0
     for data in election_data:
-        print(data["Vote Data"])
         for a in data["Vote Data"].keys():
-            print(data["Vote Data"][a]["Party"])
             if data["Vote Data"][a]["Party"] == party:
-                # a not in candidate and
-                candidate.append(a)
-
-    print(party)
-    print(candidate)
+                if a not in candidate:
+                    candidate[a] = data["Vote Data"][a]["Number of Votes"]
+                else:
+                    candidate[a] += data["Vote Data"][a]["Number of Votes"]
     options = ""
     for data in candidate:
         options = options + \
-            Markup("{ x: new Date(" + data + "), y:" + data["Number of Votes"] + "},")
+            Markup('{ label: ("' + data + '"), y:' + str(candidate[data]) + 'color: ("' + colors[count] + '")'"},")
+        count += 1
     options = options[:-1]
-    print(options)
     return options
 
 def get_candidate_options(election_data):
