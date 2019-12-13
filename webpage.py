5e85ed1ff8e0d5495d12042931cfd6e21a3379a2
Luca <lucaccorey@gmail.com>
2019-12-13 10:00:00 -0800

    Working Drilldown and new States page

--- webpage.py
+++ webpage.py
@@ -9,25 +9,50 @@ app = Flask(__name__)
 def intro():
     return render_template('Political_Demographic_Intro.html')
 
+
+
 @app.route("/StateCounty")
 def First_Page():
     with open('Primary_Elections_2016.json') as election_data:
         electionData = json.load(election_data)
         demColors = ["#37c732", "#43bfe6", "#4141f0"]
-        repColors = ["#37c732", "#43bfe6", "#4141f0", "#37c732", "#43bfe6", "#4141f0","#37c732", "#43bfe6", "#4141f0"]
+        repColors = ["#ff0400", "#ff4400", "#ff0077", "#ff01dd", "#ff015a", "#a601ff","#ff9a4d", "#ffc74d", "#ff6e4d", "#ea80ff", "#aa80ff"]
         if 'candidate' in request.args:
             return render_template('Political_Demographic_Dropdown.html', candidate=get_candidate_options(electionData),
             c_info=political_party(request.args['candidate'], electionData), c=request.args['candidate'],
             repNum=get_party("Republican", electionData), demNum=get_party("Democrat", electionData), Num=get_party("N/A", electionData),
             repNumVote=get_party_vote("Republican", electionData),demNumVote=get_party_vote("Democrat", electionData),
-            NumVote=get_party_vote("N/A", electionData), dDataPoints=get_cand_vote_data("Democrat", electionData),
-            rDataPoints=get_cand_vote_data("Republican", electionData))
+            NumVote=get_party_vote("N/A", electionData), dDataPoints=get_cand_vote_data("Democrat", electionData, demColors),
+            rDataPoints=get_cand_vote_data("Republican", electionData, repColors))
         else:
             return render_template('Political_Demographic_Dropdown.html', candidate=get_candidate_options(electionData),
             repNum=get_party("Republican", electionData), demNum=get_party("Democrat", electionData),
             Num=get_party("N/A", electionData), repNumVote=get_party_vote("Republican", electionData),
             demNumVote=get_party_vote("Democrat", electionData), NumVote=get_party_vote("N/A", electionData),
-            dDataPoints=get_cand_vote_data("Democrat", electionData, demColors), rDataPoints=get_cand_vote_data("Republican", electionData, repColors))
+            dDataPoints=get_cand_vote_data("Democrat", electionData, demColors),
+            rDataPoints=get_cand_vote_data("Republican", electionData, repColors))
+
+
+@app.route("/State")
+def state():
+    with open('Primary_Elections_2016.json') as election_data:
+        electionData = json.load(election_data)
+        if 'state' in request.args:
+            return render_template('Political_Demographic_States.html', state=get_states(electionData), s=request.args['state'])
+        else:
+            return render_template('Political_Demographic_States.html', state=get_states(electionData))
+
+def get_states(election_data):
+    state = []
+    for data in election_data:
+        if data["Location"]["State"] not in state:
+            state.append(data["Location"]["State"])
+    options = ""
+    for data in state:
+        options = options + \
+            Markup("<option value=\"" + data + "\">" + data + "</option>")
+    return options
+
 
 def get_cand_vote_data(party, election_data, colors):
     candidate = {}
@@ -42,7 +67,7 @@ def get_cand_vote_data(party, election_data, colors):
     options = ""
     for data in candidate:
         options = options + \
-            Markup('{ label: ("' + data + '"), y:' + str(candidate[data]) + 'color: ("' + colors[count] + '")'"},")
+            Markup('{ label: ("' + data + '"), y:' + str(candidate[data]) + ', color: ("' + colors[count] + '")'"},")
         count += 1
     options = options[:-1]
     return options
